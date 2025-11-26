c9a4c3af090a7a9f06a697013115fefb3fa6c7f8
nakoniecznykuba7-ui <nakoniecznykuba7@gmail.com>
2025-11-26 13:49:45 +0100

    Updated index.html

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -46,10 +46,10 @@ export default function SEO({
       <meta name="theme-color" content="#070A12" />
       <meta name="application-name" content="EarningLab" />
 
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png?v=3" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png?v=3" />
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="manifest" href="/manifest.webmanifest?v=3" />
 
       <link rel="alternate" hrefLang="pl-PL" href={hreflangURL} />
